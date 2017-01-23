2f452271160fe43a2e34551b44689db2210d15e1
Dillon <Dillon>
2017-01-23 03:04:30 -0500

    Added Anish registration css, added gender field

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,11 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
+
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
+                <div class="panel-heading" style="text-align:center">Create a new account</div>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
                         {{ csrf_field() }}
@@ -66,21 +67,41 @@
                             </div>
                         </div>
 
-                        <div class="form-group">
+                        <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
                             <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                                @if ($errors->has('password_confirmation'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password_confirmation') }}</strong>
+                                    </span>
+                                @endif
                             </div>
                         </div>
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Register
-                                </button>
+                        <div class="form-group{{ $errors->has('gender') ? ' has-error' : '' }}">
+                            <label for="gender" class="col-md-4 control-label">Gender</label>
+                              <div class="col-md-6">
+                                  <label class="radio-inline"><input type="radio" name="gender" required>Male</label>
+                                  <label class="radio-inline"><input type="radio" name="gender">Female</label>
+                                  <label class="radio-inline"><input type="radio" name="gender">Not Specified</label>
+                                  @if ($errors->has('gender'))
+                                      <span class="help-block">
+                                          <strong>{{ $errors->first('gender') }}</strong>
+                                      </span>
+                                  @endif
+                              </div>
                             </div>
+
+                        <div class="panel-footer">
+                            <button type="submit" class="btn btn-block btn-success" id="button"><span class="semibold">Sign up</span></button>
                         </div>
+
+                        <p class="text-center">
+                            <span class="text-muted">Already have an account? <a class="semibold" href="/login">Sign in here</a></span>
+                        </p>
+
                     </form>
                 </div>
             </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,23 +7,6 @@
 
         <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Bootstrap Core CSS -->
-        <link rel="stylesheet" href="theme/css/bootstrap.min.css" type="text/css">
-
-        <!-- Custom Fonts -->
-        <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
-        <link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
-        <link rel="stylesheet" href="theme/font-awesome/css/font-awesome.min.css" type="text/css">
-
-        <!-- Plugin CSS -->
-        <link rel="stylesheet" href="theme/css/animate.min.css" type="text/css">
-
-        <!-- Custom CSS -->
-        <link rel="stylesheet" href="theme/css/creative.css" type="text/css">
-
         <!-- Styles -->
         <style>
             html, body {
@@ -79,6 +62,13 @@
 
             .welcome_msg {
               font-weight:bold;
+            }
+
+            .g
+            {
+              font-size:20pt;
+              color:#61b4e7;
+              font-family:showcard Gothic;
             }
         </style>
     </head>
